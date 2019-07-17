9e8c2af58492e1832af09d38c98b29778f024479
Nonso Amadi <nonsoamadi@aol.com>
2019-07-17 15:19:32 +0100

    initialize Flask app

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
+
+# Initialize a Flask Server
+app = Flask(__name__)
+
+ # Run the Server
+if __name__ = '__main__':
+  app.run(debug=True)
